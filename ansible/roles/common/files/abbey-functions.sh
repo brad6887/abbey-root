3560da243755c8b53378f632aceff37498284020
Brad Cooke <brad6887@gmaill.com>
2026-07-01 09:16:11 -0500

    Standardize Abbey CLI tools and Ansible wrappers

--- ansible/roles/common/files/abbey-functions.sh
+++ ansible/roles/common/files/abbey-functions.sh
@@ -1,34 +1,4 @@
-# Abbey Root shell functions
-#
-# Abbey Root tool path
+# Abbey Root shell initialization
+
+# Add Abbey Root tools to PATH
 export PATH="$HOME/git/abbey-root/tools:$PATH"
-
-abbey-status() {
-  echo "========================================"
-  echo " Abbey Root Status"
-  echo "========================================"
-  echo
-  echo "Repository: ~/git/abbey-root"
-  echo
-
-  cd ~/git/abbey-root || return 1
-
-  echo "Git status:"
-  git status --short
-  echo
-
-  echo "Recent commits:"
-  git log --oneline -5
-  echo
-}
-
-abbey-git-status() {
-  cd ~/git/abbey-root || return 1
-  git status
-}
-
-abbey-git-history() {
-  cd ~/git/abbey-root || return 1
-  git log --oneline -10
-}
-
